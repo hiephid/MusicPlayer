ea72c115fd1eaa16b4d6bf4c897601dcb2795897
HID <hiepnt1804@gmail.com>
2024-01-10 12:58:58 +0700

    song list with firebase (show only, no action yet)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,11 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 }
--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.musicplayer;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicplayer.Song.adapter.SongAdapter;
+import com.example.musicplayer.Song.model.Song;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private SongAdapter songAdapter;
+    private List<Song> songList;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        db = FirebaseFirestore.getInstance();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        songList = new ArrayList<>();
+        songAdapter = new SongAdapter(songList, this, song -> {
+            // Xử lý khi một item trong RecyclerView được click
         });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(songAdapter);
+
+        readSongsFromFirestore();
     }
+
+    private void readSongsFromFirestore() {
+        db.collection("Songs").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        Song song = document.toObject(Song.class);
+                        songList.add(song);
+                        Log.d("Firestore", "Song Title: " + song.getSongTitle());
+                    }
+                    songAdapter.notifyDataSetChanged();
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("Firestore", "Error getting songs from Firestore", e);
+                });
+    }
+
 }
