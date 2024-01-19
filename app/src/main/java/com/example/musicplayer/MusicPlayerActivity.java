44d5a99af223f99217e7c2b1e002f7a51b23ef85
HID <hiepnt1804@gmail.com>
2024-01-19 09:03:02 +0700

    fix

--- app/src/main/java/com/example/musicplayer/MusicPlayerActivity.java
+++ app/src/main/java/com/example/musicplayer/MusicPlayerActivity.java
@@ -33,6 +33,7 @@ public class MusicPlayerActivity extends AppCompatActivity {
     private ImageView loopButton;
     private ImageView shuffleButton;
 
+
     private MediaPlayer mediaPlayer;
     private Handler seekBarHandler;
     private List<Song> songList;
